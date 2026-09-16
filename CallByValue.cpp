466e875bcdb83d4e14ff80a6e5bde3688ba9377a
qamruzzamamiyan <gqamruzzama@icloud.com>
2026-09-16 10:01:24 +0530

    Add files via upload

---
+++ CallByValue.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+using namespace std ;
+// void data (int x){ 
+    // x=50;
+    // cout<<x;
+// }
+inline int add(int a ,int b){
+    int c=a+b;
+    return c;
+}
+void data (string name="student"){
+    cout<<name<<endl;
+}
+int main(){
+    int d=10;
+    int e=20;
+    // cout<<add(d,e)<<endl;
+    // cout<<add(30,40);
+     data();
+     data("vikas");
+    // int m=100;
+    // data(m);
+    // cout<<m;
+}
