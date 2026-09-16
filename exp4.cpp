07ddaa611e0639753682bf60a5a7204515ad183f
qamruzzamamiyan <gqamruzzama@icloud.com>
2026-09-16 09:59:02 +0530

    Add files via upload

---
+++ exp4.cpp
@@ -0,0 +1,10 @@
+# include <bits/stdc++.h>
+using namespace std;
+int main() {
+    
+    vector<int> attendance={80,70,95,70,75,85,90,100,60,70};
+    for(auto percentage:attendance){
+        cout<<percentage<<" ";
+    }
+    
+}
